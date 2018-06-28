afb1fc5257e8c3751e2d8af805beff45522666ae
Andrew K <phpadmins@gmail.com>
2018-06-28 11:35:07 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,13 +17,15 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.status(200).send(doc);
     }, (e) => {
-        res.send(e);
+        res.status(400).send(e);
     })
 })
 
 app.listen(3000, () => {
     console.log('Server is started on port 3000');
 })
+
+module.exports = { app };
 
 // var newTodo = new Todo({
 //     text: 'Cook dinner'
