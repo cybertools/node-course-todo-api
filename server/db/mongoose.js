c2cf719c0e85c555a8f3efaaae3e3bbfc592a690
Andrew K <phpadmins@gmail.com>
2019-02-08 22:16:35 +0300

    updates

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    user: 'root',
+    pass: '@Y4_RiqMfM',
+    auth: {
+        authdb: 'admin'
+    }
+});
 
 module.exports = { mongoose };
